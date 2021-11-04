4927c9b890c8fb83d3775ca44314de85582e490e
Auriel <Amadera@protonmail.com>
2021-11-04 14:59:11 +0300

    Updated HttpPostAttribute route parameter

--- WebApp/LoginController.cs
+++ WebApp/LoginController.cs
@@ -17,7 +17,7 @@ namespace WebApp
             _db = db;
         }
 
-        [HttpPost("sign-in")]
+        [HttpPost("sign-in/{userName}")]
         public async Task<IActionResult> Login(string userName)
         {
             var account = await _db.FindByUserNameAsync(userName);
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApp
