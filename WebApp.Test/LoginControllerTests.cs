99233ae9c15435f677f47faa6e003e3af24a9295
Nikita Kuznetsov <Lutiaan@gmail.com>
2022-01-30 15:02:48 +0300

    Added test fot TODO 1

--- WebApp.Test/LoginControllerTests.cs
+++ WebApp.Test/LoginControllerTests.cs
@@ -1,17 +1,27 @@
-﻿using System.Net;
+﻿using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.Net.Http.Headers;
 using NUnit.Framework;
 
 namespace WebApp.Test;
 
 public class LoginControllerTests : MyTestBase
 {
-    public async void TestAuthCookie()
+    
+    [Test (Description = "TODO 1 Test")]
+    [TestCase("alice@mailinator.com")]
+    public async Task TestAuthCookie(string userName)
     {
+        var result = await Client.SignInAsync(userName);
+        result.EnsureSuccessStatusCode();
         
+        result.Headers.TryGetValues(HeaderNames.SetCookie, out var cookies);
+
+        Assert.True(cookies != null && cookies.All(cookie => cookie.StartsWith(".AspNetCore.Cookies=")));
     }
     
-    [Test]
+    [Test (Description = "TODO 2 Test")]
     [TestCase("not_existing@mailinator.com", HttpStatusCode.NotFound)]
     [TestCase("alice@mailinator.com", HttpStatusCode.OK)]
     [TestCase("bob@mailinator.com", HttpStatusCode.OK)]
