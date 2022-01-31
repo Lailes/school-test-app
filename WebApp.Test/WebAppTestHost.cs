92b68976e221a9b267ccc8c4250c43afbcf36be5
Nikita Kuznetsov <Lutiaan@gmail.com>
2022-01-31 12:33:50 +0300

    Indent Fix

--- WebApp.Test/WebAppTestHost.cs
+++ WebApp.Test/WebAppTestHost.cs
@@ -1,27 +1,24 @@
 using System;
 using System.Net.Http;
-using System.Threading;
-using FluentAssertions.Common;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 
-namespace WebApp.Test
+namespace WebApp.Test;
+
+public class WebAppTestHost : IDisposable
 {
-    public class WebAppTestHost : IDisposable
+    private TestServer _testServer;
+    public IServiceProvider Services => _testServer.Host.Services;
+
+    public void Start()
     {
-        private TestServer _testServer;
-        public IServiceProvider Services => _testServer.Host.Services;
-
-        public void Start()
-        {
-            var builder = WebHost.CreateDefaultBuilder();
-            builder.UseStartup<Startup>();
-            _testServer = new TestServer(builder);
-        }
-
-        public HttpClient GetClient() => _testServer.CreateClient();
-
-        public void Dispose() => _testServer?.Dispose();
+        var builder = WebHost.CreateDefaultBuilder();
+        builder.UseStartup<Startup>();
+        _testServer = new TestServer(builder);
     }
+
+    public HttpClient GetClient() => _testServer.CreateClient();
+
+    public void Dispose() => _testServer?.Dispose();
 }
--- WebApp/LoginController.cs
+++ WebApp/LoginController.cs
@@ -5,38 +5,37 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApp
+namespace WebApp;
+
+[Route("api")]
+public class LoginController : Controller
 {
-    [Route("api")]
-    public class LoginController : Controller
+    private readonly IAccountDatabase _db;
+
+    public LoginController(IAccountDatabase db)
     {
-        private readonly IAccountDatabase _db;
-
-        public LoginController(IAccountDatabase db)
-        {
-            _db = db;
-        }
-
-        [HttpPost("sign-in/{userName}")]
-        public async Task<IActionResult> Login(string userName)
-        {
-            var account = await _db.FindByUserNameAsync(userName);
-            
-            if (account == null) return NotFound();
-            //TODO_ 1: Generate auth cookie for user 'userName' with external id
-            var claims = new List<Claim> {
-                new (ClaimTypes.NameIdentifier, account.ExternalId),
-                new (ClaimTypes.Role, account.Role),
-                new (ClaimTypes.Name, account.UserName)
-            };
-            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-            await HttpContext.SignInAsync(claimsPrincipal);
-            return Ok();
-            //TODO_ 2: return 404 if user not found
-        }
-
-        [HttpGet("denied")]
-        public ActionResult AccessDenied() => Unauthorized();
+        _db = db;
     }
+
+    [HttpPost("sign-in/{userName}")]
+    public async Task<IActionResult> Login(string userName)
+    {
+        var account = await _db.FindByUserNameAsync(userName);
+            
+        if (account == null) return NotFound();
+        //TODO_ 1: Generate auth cookie for user 'userName' with external id
+        var claims = new List<Claim> {
+            new (ClaimTypes.NameIdentifier, account.ExternalId),
+            new (ClaimTypes.Role, account.Role),
+            new (ClaimTypes.Name, account.UserName)
+        };
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+        await HttpContext.SignInAsync(claimsPrincipal);
+        return Ok();
+        //TODO_ 2: return 404 if user not found
+    }
+
+    [HttpGet("denied")]
+    public ActionResult AccessDenied() => Unauthorized();
 }
